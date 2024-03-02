5bc041d646b06568f239b48329d264e7b9d76a42
sniku <mail@snek.me>
2024-03-02 06:16:07 +0100

    edit

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,9 @@
 <script>
   import Stars from './lib/StarSettings.svelte';
 
+  import Cropper from "svelte-easy-crop";
+	import { getCroppedImg } from "./lib/CanvasUtils.js"
+
   import logo       from '/pwlogo.png'
   import cardboard  from './assets/empty.png'
   import def        from './assets/shield.png'
@@ -12,14 +15,43 @@
   let deres = [ 'Bodere', 'Dandere', 'Deredere', 'Kamidere', 'Kuudere', 'Mayadere',
     'Tsundere', 'Yandere', 'Raito', 'Yami', 'Yato' ]
 
-    let image = "https://sanakan.pl/i/ss/fga432a.png";
-    let customBorder = "";
-    let showStats = false;
+  let image = "https://sanakan.pl/i/ss/fga432a.png";
+  let customBorder = "";
+  let showStats = false;
+  let editMode = false;
 
-    let starCntComp = 0;
-    let selectedStarComp;
-    let selectedBorder =  'C';
-    let selectedDere = 'Kamidere';
+  let starCntComp = 0;
+  let selectedStarComp;
+  let selectedBorder =  'C';
+  let selectedDere = 'Kamidere';
+
+  async function downloadImage() {
+    const croppedImage = await getCroppedImg(image, pixelCrop);
+    const downloadLink = document.createElement("a");
+    downloadLink.href = croppedImage;
+    downloadLink.download = "skalpelek.png";
+    downloadLink.click();
+	}
+
+  let pixelCrop, profilePicture, style, borderColor;
+
+  function previewCrop(e) {
+		pixelCrop = e.detail.pixels;
+		const { x, y, width } = e.detail.pixels;
+		const scale = 448 / width;
+
+    const hd = -y*scale;
+    const wd = -x*scale - 448 / 2;
+    const wdn = profilePicture.naturalWidth * scale;
+
+    if (pixelCrop.width < 448 || pixelCrop.height < 650) {
+      borderColor = "#ff6242";
+    } else {
+      borderColor = "#242424";
+    }
+
+		profilePicture.style=`margin: ${hd}px 0 0 ${wd}px; width: ${wdn}px;`
+	}
 </script>
 
 <main>
@@ -43,24 +75,30 @@
   </div>
 
   <div class="selector">
-    <label>Link do obrazka: <input id="url-scalp" bind:value={image} /> </label>
-    <label>Link do ramki:&nbsp;&nbsp;&nbsp;&nbsp; <input id="url-scalp" bind:value={customBorder} /> </label>
-    <label>Pokaż statystyki: <input id="show-stats" type="checkbox" bind:checked={showStats} /> </label>
+    <label>Link do obrazka: <input bind:value={image} /> </label>
+    <label>Link do ramki:&nbsp;&nbsp;&nbsp;&nbsp; <input bind:value={customBorder} /> </label>
+    <label>Pokaż statystyki: <input type="checkbox" bind:checked={showStats} /> </label>
+    <label class="exp">Tryb edycji(wip): <input type="checkbox" bind:checked={editMode} on:change={() => borderColor = "#242424"} /> </label>
   </div>
-
-  <div class="looks">
-      <img src={cardboard} class="scalp" alt="Cardboard" />
+  <div class="looks" style="border-color: {borderColor};" >
+    <img src={cardboard} class="cardboard" alt="Cardboard" />
+    {#if editMode}
+      <div class="wrapper">
+        <img bind:this={profilePicture} src={image} class="wrapper_img" alt="Scalpel" style={style}/>
+      </div>
+    {:else}
       <img src={image} class="scalp" alt="Scalpel" />
+    {/if}
       {#if customBorder}
         <img src={customBorder} class="border" alt="Border" />
       {:else}
         <img src="/borders/{selectedBorder}.png" class="border" alt="Border" />
-        <img src="/dere/{selectedDere}.png" class="dere" alt="Dere" />
+        <img src="/dere/{selectedDere}.png" class="stats" alt="Dere" />
 
         {#if showStats}
-          <img src={def} class="scalp" alt="Defense" />
-          <img src={fire} class="scalp" alt="Attack" />
-          <img src={health} class="scalp" alt="Health" />
+          <img src={def} class="stats" alt="Defense" />
+          <img src={fire} class="stats" alt="Attack" />
+          <img src={health} class="stats" alt="Health" />
         {/if}
 
         {#if starCntComp > 0}
@@ -71,7 +109,14 @@
 
       {/if}
   </div>
-
+  {#if editMode}
+  <div style="padding: 0.5em;">
+    <button type="button" on:click={async () => {downloadImage()}}>Zapisz</button>
+  </div>
+    <div class="canva">
+      <Cropper {image} crop={{x:0, y:0}} zoom={1} zoomSpeed={0.2} cropSize={{width:448, height:650}} restrictPosition={true} on:cropcomplete={previewCrop} />
+    </div>
+  {/if}
 </main>
 
 <style>
@@ -84,6 +129,33 @@
     position: relative;
     width: 475px;
     height: 667px;
+    border: 3px;
+    border-color: #242424;
+    border-style: solid;
+  }
+  .canva {
+    position: absolute;
+    width: 475px;
+    height: 667px;
+  }
+  .cardboard {
+    position: absolute;
+    top: 13px;
+    left: 13px;
+  }
+  .wrapper {
+    position: absolute;
+    top: 13px;
+    left: 13px;
+    width: 448px;
+    height: 650px;
+    overflow: hidden;
+  }
+  .exp {
+    color: #ff6242;
+  }
+  .wrapper_img {
+    position: absolute;
   }
   .scalp {
     position: absolute;
@@ -91,6 +163,7 @@
     left: 13px;
     width: 448px;
     height: auto;
+    clip-path: xywh(0 0 100% 650px);
   }
   .border {
     position: relative;
@@ -99,7 +172,7 @@
     position: absolute;
     top: 30px;
   }
-  .dere {
+  .stats {
     position: absolute;
     top: 0px;
     left: 0px;
